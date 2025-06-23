a26c848be14659485afe7787034bb8b554ca0c7e
kailas711 <kailaspsudheer@gmail.com>
2025-06-23 15:01:54 +0530

    Insertion Sort

--- insertion-sort.py
+++ insertion-sort.py
@@ -6,12 +6,12 @@ def insertsort(A):
     """
     for j in range(1, len(A)):
         key = A[j] # Insert A[J] into the sorted sequence
+        # Insert A[j] into the sorted sequence A[0...j-1].
         i = j-1
         while i>=0 and A[i]>key:
             A[ i + 1 ] = A[i]
             i = i-1
         A[ i + 1 ] = key
-
     return A
 
 print(insertsort([3,6,9,1,0]))
